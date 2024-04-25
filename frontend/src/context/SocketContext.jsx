c5bff392313773d0bb9fae9169f6dd4010994136
Chandan <cchuphal4@gmail.com>
2024-04-25 22:54:45 +0530

    updating socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chat-app-talks.onrender.com", {
         transports: ["websocket"],
         query: {
           userId: authUser._id,
